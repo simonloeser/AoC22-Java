645f275f6d8ad78bfff84e1e43b79a1182bb18c5
Simon Löser <simloeser@gmail.com>
2022-12-05 20:03:00 +0100

    feat(stupid goblin elve): day 2 part 2

--- src/DayTwo.java
+++ src/DayTwo.java
@@ -3,6 +3,9 @@ public class DayTwo {
     // Opponent: A = Rock, B = Paper, C = Scissors
     // Response: X = Rock, Y = Paper, Z = Scissors
     // Points: 1 for Rock, 2 for Paper, 3 for Scissors + 0 for lose, 3 for draw, 6 vor win
+
+    // The stupid goblin let me do all this work for nothing, so part 2:
+    // X means you need to lose, Y draw, Z win
     public static void solve() {
         String[] inputDayTwo = ReadInput.getText("DayTwo");
 
@@ -12,25 +15,25 @@ public class DayTwo {
             switch (comp.charAt(0)) {
                 case 'A' -> {
                     switch (comp.charAt(2)) {
-                        case 'X' -> points += 4;
-                        case 'Y' -> points += 8;
-                        case 'Z' -> points += 3;
+                        case 'X' -> points += 3; // pick scissors
+                        case 'Y' -> points += 4; // pick rock
+                        case 'Z' -> points += 8; // pick paper
                         default -> System.out.println("Dataset corrupted.");
                     }
                 }
                 case 'B' -> {
                     switch (comp.charAt(2)) {
-                        case 'X' -> points += 1;
-                        case 'Y' -> points += 5;
-                        case 'Z' -> points += 9;
+                        case 'X' -> points += 1; // pick rock
+                        case 'Y' -> points += 5; // pick paper
+                        case 'Z' -> points += 9; // pick scissors
                         default -> System.out.println("Dataset corrupted.");
                     }
                 }
                 case 'C' -> {
                     switch (comp.charAt(2)) {
-                        case 'X' -> points += 7;
-                        case 'Y' -> points += 2;
-                        case 'Z' -> points += 6;
+                        case 'X' -> points += 2; // pick paper
+                        case 'Y' -> points += 6; // pick scissors
+                        case 'Z' -> points += 7; // pick rock
                         default -> System.out.println("Dataset corrupted.");
                     }
                 }
