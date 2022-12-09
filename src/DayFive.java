3c17756979fd90fd9467dd0d84052cd15baa3f51
Simon Löser <simloeser@gmail.com>
2022-12-09 23:30:04 +0100

    feat: day 5 fill stacks according to base config

--- src/DayFive.java
+++ src/DayFive.java
@@ -11,7 +11,7 @@ public class DayFive {
 
     private static String partOne(String[] input) {
         ArrayList<String> startConfigList = new ArrayList<>();
-        ArrayList<Stack<String>> stackList= new ArrayList<>();
+        ArrayList<Stack<String>> stackList = new ArrayList<>();
         boolean startFinished = false;
         for (String line : input) {
             if (line.equals("") && !startFinished) {
@@ -19,12 +19,23 @@ public class DayFive {
 
                 String s = startConfigList.get(startConfigList.size() - 1).trim();
                 int stacks = Integer.parseInt(s.substring(s.lastIndexOf(" ") + 1));
+                startConfigList.remove(startConfigList.size() - 1);
 
-                // Fill list with needed number of stacks
+                // Fill stacks and save them
                 for (int i = 0; i < stacks; i++) {
-                    stackList.add(new Stack<>());
+                    Stack<String> newStack = new Stack<>();
+                    for (int j = 0; j < startConfigList.size(); j++) {
+                        int item = startConfigList.size() - 1 - j;
+                        if (startConfigList.get(item).startsWith("[")) {
+                            newStack.push(String.valueOf(startConfigList.get(item).charAt(1)));
+                            System.out.println(newStack);
+                        }
+                        if (startConfigList.get(item).length() >= 4) {
+                            startConfigList.set(item, startConfigList.get(item).substring(4));
+                        }
+                    }
+                    stackList.add(newStack);
                 }
-                // TODO: Fill stacks
             } else {
                 if (!startFinished) {
                     startConfigList.add(line);
