19072f63b37a351c2a7250eb4bfdbfb48cde1430
Simon Löser <simloeser@gmail.com>
2022-12-09 12:33:47 +0100

    feat: day3 part2

--- src/DayThree.java
+++ src/DayThree.java
@@ -6,6 +6,7 @@ public class DayThree {
         String priority = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         System.out.println("Part 1 - sum of doubled items: " + partOne(input, priority));
+        System.out.println("Part 2 - sum of badge priorities: " + partTwo(input, priority));
     }
 
     public static int partOne(String[] input, String priority) {
@@ -28,5 +29,43 @@ public class DayThree {
             }
         });
         return sum[0];
+    }
+
+    public static int partTwo(String[] input, String priority) {
+        int sum = 0;
+        int count = 0;
+        boolean done = false;
+        String first = "";
+        String second = "";
+        for (String line : input) {
+            switch (count) {
+                case 0 -> first = line;
+                case 1 -> second = line;
+                case 2 -> {
+                    for (int i = 0; i < first.length(); i++) {
+                        for (int j = 0; j < second.length(); j++) {
+                            for (int k = 0; k < line.length(); k++) {
+                                if (first.charAt(i) == second.charAt(j) && second.charAt(j) == line.charAt(k)) {
+                                    sum += priority.indexOf(line.charAt(k)) + 1;
+                                    done = true;
+                                    break;
+                                }
+                            }
+                            if (done) {
+                                break;
+                            }
+                        }
+                        if (done) {
+                            done = false;
+                            break;
+                        }
+                    }
+                    count = -1;
+                }
+                default -> System.out.println("Failure");
+            }
+            count++;
+        }
+        return sum;
     }
 }
