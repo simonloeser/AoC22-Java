bb0e0795d7468b70367f9f3a2b6c9433c55bf9b4
Simon Löser <simloeser@gmail.com>
2022-12-11 21:20:29 +0100

    fix: day 8 2-dim-array

--- src/DayEight.java
+++ src/DayEight.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.Objects;
-
 public class DayEight {
     public static void solve() {
         String[] input = ReadInput.getText("DayEight");
@@ -9,19 +6,15 @@ public class DayEight {
     }
 
     private static int partOne(String[] input) {
-        int visibleTrees = input.length * 2 + input[0].length() * 2 - 4;
+        int visibleTrees = input.length * 2 - 1 + input[0].length() * 2 - 4;
         int[][] layout = new int[input[0].length()][input.length];
 
         // Fill 2-dimensional array from input
         for (int i = 0; i < input.length; i++) {
-            if (input[i].equals("")) {
-                break;
-            }
-            for (int j = 0; i < input[i].length(); j++) {
+            for (int j = 0; j < input[i].length(); j++) {
                 layout[i][j] = Integer.parseInt(String.valueOf(input[i].charAt(j)));
             }
         }
-        System.out.println(Arrays.toString(layout));
         return visibleTrees;
     }
 
