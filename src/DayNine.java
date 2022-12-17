ee96a5369844745681fadf5dcec828c13d11246e
Simon Löser <simloeser@gmail.com>
2022-12-17 14:29:39 +0100

    fix: day 9 part 1 wtf

--- src/DayNine.java
+++ src/DayNine.java
@@ -6,13 +6,19 @@ public class DayNine {
     }
 
     private static int partOne(String[] input) {
-        String[][] moves = new String[input.length * 2][input.length * 2];
-        boolean[][] visited = new boolean[input.length * 2][input.length * 2];
+        String[][] moves = new String[input.length / 5][input.length / 5];
+        boolean[][] visited = new boolean[input.length / 5][input.length / 5];
 
-        int headX = input.length;
-        int headY = input.length;
-        int tailX = input.length;
-        int tailY = input.length;
+        for (int i = 0; i < moves.length; i++) {
+            for (int j = 0; j < moves[0].length; j++) {
+                moves[i][j] = ".";
+            }
+        }
+
+        int headX = moves.length / 2;
+        int headY = moves[0].length / 2;
+        int tailX = moves.length / 2;
+        int tailY = moves[0].length / 2;
 
         moves[headX][headY] = "H";
         visited[tailX][tailY] = true;
@@ -23,49 +29,62 @@ public class DayNine {
                 case 'U' -> {
                     for (int i = 1; i <= bounds; i++) {
                         moves[headX][headY] = "#";
+                        moves[tailX][tailY] = "#";
                         headX--;
                         if (headX != tailX && headY != tailY) {
                             tailX = headX + 1;
                             tailY = headY;
                         }
+                        visited[tailX][tailY] = true;
                     }
                 }
                 case 'R' -> {
                     for (int i = 1; i <= bounds; i++) {
                         moves[headX][headY] = "#";
+                        moves[tailX][tailY] = "#";
                         headY++;
                         if (headX != tailX && headY != tailY) {
                             tailX = headX;
                             tailY = headY - 1;
                         }
+                        visited[tailX][tailY] = true;
                     }
                 }
                 case 'D' -> {
                     for (int i = 1; i <= bounds; i++) {
                         moves[headX][headY] = "#";
+                        moves[tailX][tailY] = "#";
                         headX++;
                         if (headX != tailX && headY != tailY) {
                             tailX = headX - 1;
                             tailY = headY;
                         }
+                        visited[tailX][tailY] = true;
                     }
                 }
                 case 'L' -> {
                     for (int i = 1; i <= bounds; i++) {
                         moves[headX][headY] = "#";
+                        moves[tailX][tailY] = "#";
                         headY--;
                         if (headX != tailX && headY != tailY) {
                             tailX = headX;
                             tailY = headY + 1;
                         }
+                        visited[tailX][tailY] = true;
                     }
                 }
                 default -> System.out.println("Dataset corrupted");
             }
             moves[tailX][tailY] = "T";
             moves[headX][headY] = "H";
-            visited[tailX][tailY] = true;
         }
+        /*for (String[] move1 : moves) {
+            for (String move2 : move1) {
+                System.out.print(move2);
+            }
+            System.out.println();
+        }*/
         int count = 0;
         for (boolean[] visit : visited) {
             for (boolean visit2 : visit) {
